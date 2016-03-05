4d0abf333d24666ea0256d12c8a57c54d2392c30
Mario Araujo <mgiusti.araujo@gmail.com>
2016-03-05 15:20:27 -0300

    Constant and project reference

---
+++ SMC.WebApi/SMC.WebApi/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMC.WebApi.Constants
+{
+    public static class Constants
+    {
+        public const string Token = "8L7F2Rx0Rph6";
+    }
+}
